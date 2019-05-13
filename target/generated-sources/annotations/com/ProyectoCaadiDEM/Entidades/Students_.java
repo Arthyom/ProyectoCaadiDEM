997ad748f152d83e877ea9ed96315cf7a4af4311
frodoBang <frodobang@frodoBang>
2019-05-12 21:51:47 -0500

    carga de visitas desde excel

--- target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Students_.java
+++ target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Students_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2019-03-19T14:02:42")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2019-05-12T16:03:01")
 @StaticMetamodel(Students.class)
 public class Students_ { 
 
